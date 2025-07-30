4907e92a2e3531af795f183da2a1f8f8120fdd58
Safi32 <shaheershahid75@gmail.com>
2025-07-31 01:11:11 +0500

    Fixing

--- src/api/app_config/entities/app_config.entity.ts
+++ src/api/app_config/entities/app_config.entity.ts
@@ -7,7 +7,7 @@
 import mongoose from "mongoose";
  import {RegisterStatus} from "../../../core/utils/enums";
 import {VRoomsIcon} from "../../../core/utils/interfaceces";
-import { version } from '../../package.json'; 
+const { version } = require('../../../../package.json'); 
 
 export interface IAppConfig {
     _id: string,
